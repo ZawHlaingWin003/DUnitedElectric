6756b3514de4a08a05985fb4fc646a08ba9b420d
ZawHlaingWin007 <zawhlaingwin.mti@gmail.com>
2024-12-24 17:28:30 +0700

    fixed: Modified vite config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     plugins: [
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                home: "index.html",
+                about: "about.html",
+                contact: "contact.html",
+                services: "services.html",
+                shop: "shop.html",
+            },
+        }
+    },
 })
